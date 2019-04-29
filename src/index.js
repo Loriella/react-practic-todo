da5d68984855e026e925432083655ec314be2a18
Loriella <loris.orlova@gmail.com>
2019-04-29 16:54:39 +0300

    Adds CSS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './components/todo-list'
-import AppHeader from './components/app-header'
-import SearchPanel from './components/search-panel'
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 import ItemStatusFilter from './components/item-status-filter';
 
+import './index.css';
+
 const App = () => {
+
   const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1},
-    { label: 'Make Awesome App', important: true, id: 2},
-    { label: 'Have a lunch', important: false, id: 3}
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
   ];
 
   return (
@@ -19,9 +23,11 @@ const App = () => {
         <SearchPanel />
         <ItemStatusFilter />
       </div>
+
       <TodoList todos={todoData} />
     </div>
-  )
+  );
 };
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
