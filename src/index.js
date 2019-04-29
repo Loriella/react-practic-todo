d76674c4cd3d2e6f8e5ac6132c8b8c60d624fd29
Loriella <loris.orlova@gmail.com>
2019-04-29 16:08:52 +0300

    Creates array todoData

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import AppHeader from './components/app-header'
 import SearchPanel from './components/search-panel'
 
 const App = () => {
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1},
+    { label: 'Make Awesome App', important: true, id: 2},
+    { label: 'Have a lunch', important: false, id: 3}
+  ];
 
   return (
     <div>
       <AppHeader/>
       <SearchPanel/>
-      <TodoList/>
+      <TodoList todos={todoData}/>
     </div>
   )
 };
