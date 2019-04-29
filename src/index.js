c0209debc0ad5f1a8293364ff2f870a219dbb365
Loriella <loris.orlova@gmail.com>
2019-04-29 15:45:17 +0300

    Creates app-header component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './components/todo-list'
+import AppHeader from './components/app-header'
 
-const AppHeader = () => {
-  return (
-    <h1>My Todo list</h1>
-  )
-};
 
 const SearchPanel = () => {
   return <input placeholder="search"/>
