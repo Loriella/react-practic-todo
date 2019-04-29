29f6c820e71130c27773f3a7974673e0db2ab843
Loriella <loris.orlova@gmail.com>
2019-04-29 15:47:27 +0300

    Creates search-panel component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './components/todo-list'
 import AppHeader from './components/app-header'
-
-
-const SearchPanel = () => {
-  return <input placeholder="search"/>
-};
+import SearchPanel from './components/search-panel'
 
 const App = () => {
 
