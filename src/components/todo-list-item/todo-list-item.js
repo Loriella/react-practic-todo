b1fbc273da0f32e9af2e75b2dea570f915f44c50
Loriella <loris.orlova@gmail.com>
2019-04-30 16:48:31 +0300

    add onClick for item-status-filter

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,32 +3,55 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  render() {
-    const {label, important = false} = this.props;
+  state = {
+    done: false,
+    important: false
+  };
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    })
+  };
+
+  onMarkImportant = () => {
+    this.setState({
+      important: true
+    })
+  };
+
+  render() {
+    const {label} = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done'
+    }
+
+    if (important) {
+      classNames += ' important'
+    }
 
     return (
-      <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          onClick={ this.onLabelClick } >
+          {label}
+        </span>
+
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}>
+          <i className="fa fa-exclamation"/>
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o"/>
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation"/>
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o"/>
-      </button>
-    </span>
     )
   }
 
